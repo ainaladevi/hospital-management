3d54ccb22b19caf93ce8bd170232993da9ba075e
ainaladevi <ainaladevi@gmail.com>
2026-06-24 12:44:44 +0530

    pharma folder committed

---
+++ src/pharma-app/src/App.jsx
@@ -0,0 +1,142 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+import AllPage from './Pharma/AllPage';
+import DispensingPage from './Pharma/DispensingPage';
+import ExpiringPage from './Pharma/ExpiringPage';
+import InventoryPage from './Pharma/InventoryPage';
+import LowStockPage from './Pharma/LowStockPage';
+import PharmacyDashboardPage from './Pharma/PharmacyDashboardPage';
+import PrescriptionsPage from './Pharma/PrescriptionsPage';
+import './Pharma/Styling.css'
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      {/* <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section> */}
+        <Routes>
+          <Route path="/" element={<PharmacyDashboardPage />}></Route>
+          <Route path="/PrescriptionPage" element={<PrescriptionsPage />}></Route>
+          <Route path="/InventoryPage" element={<InventoryPage />}></Route>
+          <Route path="/DispensingPage" element={<DispensingPage />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/allpage" element={<AllPage />}></Route>
+          <Route path="/lowstock" element={<LowStockPage />}></Route>
+          <Route path="/expiring" element={<ExpiringPage />}></Route>
+        </Routes>
+    </>
+  )
+}
+
+export default App
